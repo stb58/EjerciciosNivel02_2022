26df4d2d4dda7d835b360e150df33de123002458
sergi <sergi@DESKTOP-LTNM675.mshome.net>
2022-01-14 19:21:11 +0100

    Commit inicial

---
+++ src/ejerciciosnivel02/EjerciciosNivel02.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejerciciosnivel02;
+
+/**
+ *
+ * @author sergi
+ */
+public class EjerciciosNivel02 {
+
+   public boolean esEscaleraPalabras(char[][] listaPalabras){
+       
+       
+       return true;
+   } 
+  
+    public static void main(String[] args) {
+        char [][] listaPalabras = {
+             {'P', 'A', 'T', 'A'}, 
+            {'P', 'A', 'T', 'O'}, 
+            {'R', 'A', 'T', 'O'}, 
+            {'R', 'A', 'M', 'O'}, 
+            {'G', 'A', 'M', 'O'}, 
+            {'G', 'A', 'T', 'O'}, 
+            {'M', 'A', 'T', 'O'}, 
+        };
+    }
+    
+}
