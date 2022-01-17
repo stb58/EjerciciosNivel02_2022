d66831561375f58cf008b14db9450de055c05108
sergi <sergi@DESKTOP-LTNM675.mshome.net>
2022-01-17 19:09:06 +0100

    Cadenas ADN completado

--- src/ejerciciosnivel02/EjerciciosNivel02.java
+++ src/ejerciciosnivel02/EjerciciosNivel02.java
@@ -24,6 +24,21 @@ public class EjerciciosNivel02 {
        }
        return true;
    } 
+   private int costeErroresADN (String cadena1, String cadena2){
+       int contador=0;
+       for(int i=0; i<cadena1.length();i++){
+       if(cadena1.charAt(i)=='-'|| cadena2.charAt(i)=='-'){
+           contador++;
+           contador++;
+       } else if (cadena1.charAt(i)== 'A' && cadena2.charAt(i) !='T' 
+               || cadena1.charAt(i)== 'C' && cadena2.charAt(i) !='G'
+               || cadena1.charAt(i)== 'T' && cadena2.charAt(i) !='A'
+               || cadena1.charAt(i)== 'G' && cadena2.charAt(i) !='C' )
+           contador++;
+           }
+      return contador;
+       
+   }
   
     public static void main(String[] args) {
         char [][] listaPalabras = {
@@ -37,6 +52,14 @@ public class EjerciciosNivel02 {
         };
         EjerciciosNivel02 e = new EjerciciosNivel02();
         //System.out.println(e.esEscaleraPalabras(listaPalabras));
+        System.out.println(e.costeErroresADN("ACGT", "TGCA"));
+        System.out.println(e.costeErroresADN("A-C-G-T-ACGT", "TTGGCCAATGCA"));
+        System.out.println(e.costeErroresADN("AAAAAAAA", "TTTATTTT"));
+        System.out.println(e.costeErroresADN("GATTACA", "CTATT-T"));
+        System.out.println(e.costeErroresADN("CAT-TAG-ACT", "GTATATCCAAA"));
+        System.out.println(e.costeErroresADN("--------", "ACGTACGT"));
+        System.out.println(e.costeErroresADN("TAATAA", "ATTATT"));
+        System.out.println(e.costeErroresADN("GGGA-GAATATCTGGACT", "CCCTACTTA-AGACCGGT"));
     }
     
 }
